6200458efdd657832e5d8a8488e58349d7d7b59c
Yur1M3lo <yurimelo@ufrj.br>
2023-07-18 15:29:16 -0300

    iniciando projeto e adicionando structs para criar a estrutura da fila

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct No
+{
+    int info;
+    struct No* proximo;
+};
+
+struct Fila
+{
+    struct No* inicio;
+    struct No* fim;
+    int tamanho;
+};
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
